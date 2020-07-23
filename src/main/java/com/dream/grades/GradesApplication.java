bbd76ecf744fd4c0b00ffa76f023dc2c986639db
zhengweiwei <164431272@qq.com>
2020-07-23 14:55:22 +0800

    修改配置文件

--- src/main/java/com/dream/grades/GradesApplication.java
+++ src/main/java/com/dream/grades/GradesApplication.java
@@ -1,12 +1,15 @@
 package com.dream.grades;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+
 @SpringBootApplication
+@MapperScan("com.dream.grades.dao.mapper")
 public class GradesApplication {
 	public static final Logger logger = LoggerFactory.getLogger(GradesApplication.class);
 	public static void main(String[] args) {
